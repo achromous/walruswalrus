68fb3a46f9f58a53e6d03b13282e0ec05f17f9c9
conor maguire <conor@achromo.us>
2016-12-24 11:59:54 -0500

    again

--- index.js
+++ index.js
@@ -45,7 +45,12 @@ io.sockets.on('connection', function(socket) {
       socket.emit('joined', room, socket.id);
       io.sockets.in(room).emit('ready');
     } else { // max two clients
-      socket.emit('full', room);
+      log('Client ID ' + socket.id + ' joined room ' + room);
+      io.sockets.in(room).emit('join', room);
+      socket.join(room);
+      socket.emit('joined', room, socket.id);
+      io.sockets.in(room).emit('ready');
+      // socket.emit('full', room);
     }
   });
 
